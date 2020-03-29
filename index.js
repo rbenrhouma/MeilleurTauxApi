568b56d7dc12ce612d29f55b77126d195e3e7ff2
Ridha <rbenrhouma2@gmail.com>
2020-03-29 18:41:26 +0200

    cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
 const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/devis", {
   useNewUrlParser: true,
