ba25f4628fa9fe6b2783ec40ca72ee511e7a75e6
Ridha <rbenrhouma2@gmail.com>
2020-03-29 12:06:41 +0200

    test

--- routes/_devis.js
+++ routes/_devis.js
@@ -25,53 +25,7 @@ router.get("/", async (req, res) => {
 
 // Route de create d'un devis
 router.post("/save", async (req, res) => {
-  // Verification de la validation du devis
-  if (
-    req.fields.typeBien &&
-    req.fields.montant &&
-    req.fields.country &&
-    req.fields.zipCode &&
-    req.fields.email &&
-    req.fields.total !== undefined
-  ) {
-    //Creation d'un nouveau Devis
-    const newDevis = new Devis({
-      //Generation number alléatoire
-      key: generator.generate({
-        length: 8,
-        numbers: true,
-        uppercase: false,
-        exclude: "abcdefghijklmnopqrstuvwxyz"
-      }),
-      typeBien: req.fields.typeBien,
-      etatBien: req.fields.etatBien,
-      usageBien: req.fields.usageBien,
-      situationUser: req.fields.situationUser,
-      typeBienLib: req.fields.typeBienLib,
-      etatBienLib: req.fields.etatBienLib,
-      usageBienLib: req.fields.usageBienLib,
-      situationUserLib: req.fields.situationUserLib,
-      country: req.fields.country,
-      zipCode: req.fields.zipCode,
-      montant: req.fields.montant,
-      travaux: req.fields.travaux,
-      notaire: req.fields.notaire,
-      total: req.fields.total,
-      email: req.fields.email
-    });
-    // Sauvegarde de devis + envois de mail
-
-    try {
-      await newDevis.save();
-      // envois au client
-      res.send(newDevis);
-    } catch (err) {
-      console.log(err);
-      res.status(400).send({ message: "Error during saving process" });
-    }
-  } else {
-    res.status(400).send({ message: "Some parameters are missing" });
-  }
+  res.send("Devis !!!");
 });
 
 // router.put("/update", async (req, res) => {});
